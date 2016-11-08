4ac76d6b2261825cf81724fd2ba4dbcaaac49db6
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-11-07 18:56:40 -0600

    intial commit - also contains foundation of UI

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Joe Janiszewski on 11/7/16.
+//  Copyright © 2016 Joe Janiszewski. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
