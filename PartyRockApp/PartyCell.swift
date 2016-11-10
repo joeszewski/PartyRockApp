d318b0bb06f9ce2405c08907122ff1be05f0feca
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-11-10 15:59:20 -0600

    Added table view IBOutlets, sample data for video urls, images, and titles, and added async thread for url image loading

---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,42 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Joe Janiszewski on 11/8/16.
+//  Copyright © 2016 Joe Janiszewski. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+    
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            }catch {
+                //handle the error
+            }
+        }
+        
+    }
+
+}
