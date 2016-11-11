8a33611ba92d71764bce6e733d5636877c3b9c04
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-11-10 19:35:32 -0600

    Created webView and titleLbl IBOutlets on VideoVC.  Added back button on VideoVC.  Loaded video HTML into web view.

--- PartyRockApp/VideoVC.swift
+++ PartyRockApp/VideoVC.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class VideoVC: UIViewController {
-
+    
+    @IBAction func BackBtnPressed(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    private var _partyRock: PartyRock!
+    
+    var partyRock: PartyRock {
+        get {
+            return _partyRock
+        } set {
+            _partyRock = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleLbl.text = partyRock.videoTitle
+        webView.loadHTMLString(partyRock.videoURL, baseURL: nil)
     }
-
+    
     
 
     /*
